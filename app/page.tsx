9b3a383ae701cd64ff6bef1fae3b9d44a76ed877
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2024-09-22 22:55:32 +0700

    feat: added shadergradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,17 @@
-import Image from "next/image";
+import React from "react";
+import dynamic from "next/dynamic";
+
+const NoSSRShaderGradient = dynamic(
+	() => import("@/components/organisms/shader-gradient"),
+	{ ssr: false }
+);
 
 export default function Home() {
 	return (
 		<main>
-			<section className="relative h-screen w-full object-cover object-center">
-				<video
-					controls={false}
-					loop={true}
-					autoPlay={true}
-					muted={true}
-					playsInline
-					className="h-full w-full object-cover object-center"
-				>
-					<source src="./background.webm" type="video/webm"></source>
-				</video>
-				<div className="absolute inset-0 flex h-full w-full">
-					<div className="lg:px-content-padding-lg">
-						<h1 className="text-4xl text-white">Crafting</h1>
-					</div>
-				</div>
-			</section>
+			<main style={{ height: "100vh", position: "relative" }}>
+				<NoSSRShaderGradient />
+			</main>
 		</main>
 	);
 }
---
+++ components/organisms/shader-gradient.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from "react";
+import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
+import * as reactSpring from "@react-spring/three";
+import * as drei from "@react-three/drei";
+import * as fiber from "@react-three/fiber";
+
+export default function ShaderGradientComponent() {
+	return (
+		<ShaderGradientCanvas
+			importedfiber={{
+				...fiber,
+				...drei,
+				...reactSpring,
+			}}
+			style={{
+				position: "absolute",
+				top: 0,
+				width: "100%",
+				height: "100%",
+				zIndex: -1, // Ensures it's behind other content
+				overflow: "hidden", // Prevents zoom or scroll affecting the canvas
+			}}
+		>
+			{/* Add ShaderGradient with your parameters */}
+			<ShaderGradient
+				type="waterPlane"
+				uStrength={1.5}
+				uDensity={1.5}
+				color1="#6917B0"
+				color2="#FF5E29"
+				color3="#212121"
+				reflection={10}
+				grain="off"
+				brightness={3}
+				animate="on"
+				uSpeed={0.2}
+				rangeStart={0}
+				rangeEnd={40}
+				cDistance={2.5}
+				cAzimuthAngle={180}
+				cPolarAngle={80}
+				positionX={0}
+				positionY={0}
+				positionZ={0}
+				rotationX={50}
+				rotationY={0}
+				rotationZ={-60}
+				zoomOut={false}
+				enableTransition={false}
+			/>
+
+			{/* You can add OrbitControls, Environment, or any drei components here */}
+		</ShaderGradientCanvas>
+	);
+}
